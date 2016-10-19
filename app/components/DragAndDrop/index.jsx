191d24e92b7f9faa3a6267f08c476e036bfb1c24
hackape <muse.vincent@gmail.com>
2016-10-19 13:30:21 +0800

    wrap switch-case procedure into function call

--- app/components/DragAndDrop/index.jsx
+++ app/components/DragAndDrop/index.jsx
@@ -56,79 +56,7 @@ class DragAndDrop extends Component {
 
     switch (source.type) {
       case 'TAB':
-        if (target.type !== 'PANE') break
-
-        const {top, left, right, bottom, height, width} = target.rect
-        const leftRule = left + width/3
-        const rightRule = right - width/3
-        const topRule = top + height/3
-        const bottomRule = bottom - height/3
-
-        let overlayPos
-        if (oX < leftRule) {
-          overlayPos = 'left'
-        } else if (oX > rightRule) {
-          overlayPos = 'right'
-        } else if (oY < topRule) {
-          overlayPos = 'top'
-        } else if (oY > bottomRule) {
-          overlayPos = 'bottom'
-        } else {
-          overlayPos = 'center'
-        }
-
-        // nothing changed, stop here
-        if (meta && meta.paneSplitDirection === overlayPos) return
-
-        const heightTabBar = target.DOMNode.querySelector('.tab-bar').offsetHeight
-        let overlay
-        switch (overlayPos) {
-          case 'left':
-            overlay = {
-              top: top + heightTabBar,
-              left: left,
-              width: width/2,
-              height: height - heightTabBar
-            }
-            break
-          case 'right':
-            overlay = {
-              top: top + heightTabBar,
-              left: left + width/2,
-              width: width/2,
-              height: height - heightTabBar
-            }
-            break
-          case 'top':
-            overlay = {
-              top: top + heightTabBar,
-              left: left,
-              width: width,
-              height: (height - heightTabBar)/2
-            }
-            break
-          case 'bottom':
-            overlay = {
-              top: top + (height + heightTabBar)/2,
-              left: left,
-              width: width,
-              height: (height - heightTabBar)/2
-            }
-            break
-          default:
-            overlay = {
-              top: top + heightTabBar,
-              left: left,
-              width: width,
-              height: height - heightTabBar
-            }
-        }
-
-        dispatch(updateDragOverMeta({
-          paneSplitDirection: overlayPos,
-          paneLayoutOverlay: overlay
-        }))
-        break
+        return this.dragTabOverPane(e, target)
 
       default:
     }
@@ -138,6 +66,83 @@ class DragAndDrop extends Component {
   }
 
   onDragEnd = (e) => {
+  }
+
+  dragTabOverPane (e, target) {
+    if (target.type !== 'PANE') return
+    const {meta, dispatch} = this.props
+    const [oX, oY] = [e.pageX, e.pageY]
+
+    const {top, left, right, bottom, height, width} = target.rect
+    const leftRule = left + width/3
+    const rightRule = right - width/3
+    const topRule = top + height/3
+    const bottomRule = bottom - height/3
+
+    let overlayPos
+    if (oX < leftRule) {
+      overlayPos = 'left'
+    } else if (oX > rightRule) {
+      overlayPos = 'right'
+    } else if (oY < topRule) {
+      overlayPos = 'top'
+    } else if (oY > bottomRule) {
+      overlayPos = 'bottom'
+    } else {
+      overlayPos = 'center'
+    }
+
+    // nothing changed, stop here
+    if (meta && meta.paneSplitDirection === overlayPos) return
+
+    const heightTabBar = target.DOMNode.querySelector('.tab-bar').offsetHeight
+    let overlay
+    switch (overlayPos) {
+      case 'left':
+        overlay = {
+          top: top + heightTabBar,
+          left: left,
+          width: width/2,
+          height: height - heightTabBar
+        }
+        break
+      case 'right':
+        overlay = {
+          top: top + heightTabBar,
+          left: left + width/2,
+          width: width/2,
+          height: height - heightTabBar
+        }
+        break
+      case 'top':
+        overlay = {
+          top: top + heightTabBar,
+          left: left,
+          width: width,
+          height: (height - heightTabBar)/2
+        }
+        break
+      case 'bottom':
+        overlay = {
+          top: top + (height + heightTabBar)/2,
+          left: left,
+          width: width,
+          height: (height - heightTabBar)/2
+        }
+        break
+      default:
+        overlay = {
+          top: top + heightTabBar,
+          left: left,
+          width: width,
+          height: height - heightTabBar
+        }
+    }
+
+    dispatch(updateDragOverMeta({
+      paneSplitDirection: overlayPos,
+      paneLayoutOverlay: overlay
+    }))
   }
 }
 
