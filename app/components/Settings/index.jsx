b8e0115edeacbf237b721e3d746cf8028272b5cd
hackape <muse.vincent@gmail.com>
2016-11-18 16:21:51 +0800

    renaming variables

--- app/components/Settings/index.jsx
+++ app/components/Settings/index.jsx
@@ -6,11 +6,11 @@ import _ from 'lodash'
 
 
 let SettingState = {
-  activeItem: 'EDITOR',
-  itemLabels: ['GENERAL', 'EDITOR'],
-  items: {
+  activeTabId: 'EDITOR',
+  tabIds: ['GENERAL', 'EDITOR'],
+  tabs: {
     'GENERAL': {
-      label: 'GENERAL',
+      id: 'GENERAL',
       language: {
         value: 'english',
         options: ['english', 'chinese']
@@ -21,16 +21,16 @@ let SettingState = {
       }
     },
     'EDITOR': {
-      label: 'EDITOR',
+      id: 'EDITOR',
       items: [{
-        label: 'Keyboard Mode',
+        name: 'Keyboard Mode',
         value: '',
         options: ['default', 'vim', 'emacs']
       }, {
-        label: 'Font Size',
+        name: 'Font Size',
         value: 14
       }, {
-        label: 'Font Family',
+        name: 'Font Family',
         value: '',
         options: ['Consolas', 'Courier', 'Courier New', 'Menlo']
       }]
@@ -39,23 +39,23 @@ let SettingState = {
 }
 
 const SettingsView = (_props) => {
-  const {activeItem, itemLabels, items} = SettingState
+  const {activeTabId, tabIds, tabs} = SettingState
 
   return (
     <div className='settings-container'>
       <div className='settings-header'>
-        <div className='nav-header'>Settings</div>
-        <ul className='tabs'>
-          {itemLabels.map(itemLabel =>
-            <li key={itemLabel}
-              className={cx('nav-item', {'active': itemLabel === activeItem})}
-              onClick={e=>''} >{itemLabel}</li>
+        <div className='tab-bar-header'>Settings</div>
+        <ul className='tab-bar-tabs'>
+          {tabIds.map(tabId =>
+            <li key={tabId}
+              className={cx('tab-bar-item', {'active': tabId === activeTabId})}
+              onClick={e=>''} >{tabId}</li>
           )}
         </ul>
       </div>
       <div className='settings-content'>
         <div className='settings-content-container'>
-          <SettingsContent content={items[activeItem]} />
+          <SettingsContent content={tabs[activeTabId]} />
         </div>
       </div>
     </div>
@@ -63,7 +63,7 @@ const SettingsView = (_props) => {
 }
 
 const SettingsContent = ({content}) => {
-  switch (content.label) {
+  switch (content.id) {
     case 'GENERAL':
     default:
       return <GeneralSettings {...content} />
@@ -77,9 +77,9 @@ const FormGroupFactory = ({settingItem}) => {
   if (settingItem.options && _.isArray(settingItem.options)) {
     return (
       <div className='form-group'>
-        <label>{settingItem.label}</label>
+        <label>{settingItem.name}</label>
         <select className="form-control"
-          onChange={e => ''}
+          onChange={e => console.log(e.target.value)}
           value={settingItem.value} >
           {settingItem.options.map(option =>
             _.isObject(option) ?
@@ -91,7 +91,7 @@ const FormGroupFactory = ({settingItem}) => {
   } else if (settingItem.isCheckbox) {
     return (
       <div className='form-group'>
-        <label>{settingItem.label}</label>
+        <label>{settingItem.name}</label>
         <input className="form-control"
           type='checkbox'
           value={settingItem.value} />
@@ -99,7 +99,7 @@ const FormGroupFactory = ({settingItem}) => {
   } else {
     return (
       <div className='form-group'>
-        <label>{settingItem.label}</label>
+        <label>{settingItem.name}</label>
         <input className="form-control"
           type='text'
           value={settingItem.value} />
@@ -140,7 +140,7 @@ const EditorSettings = (props) => {
     <div>
       <h2 className='settings-content-header'>Editor Settings</h2>
       {props.items.map(settingItem =>
-        <FormGroupFactory key={settingItem.label} settingItem={settingItem} />
+        <FormGroupFactory key={settingItem.name} settingItem={settingItem} />
       )}
     </div>
   )
