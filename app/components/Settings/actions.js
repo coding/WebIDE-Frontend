ca81f770098fab95fc547b47e3474c81de6f9c69
hackape <muse.vincent@gmail.com>
2016-11-23 09:54:26 +0800

    constructing the settings view, hook it up with SettingState

---
+++ app/components/Settings/actions.js
@@ -0,0 +1,9 @@
+import { createAction } from 'redux-actions'
+
+export const SETTING_ACTIVATE_TAB = 'SETTING_ACTIVATE_TAB'
+export const activateSettingsTab = createAction(SETTING_ACTIVATE_TAB)
+
+export const SETTING_UPDATE_FIELD = 'SETTING_UPDATE_FIELD'
+export const updateSettingItem = createAction(SETTING_UPDATE_FIELD,
+  (domain, fieldName, value) => ({domain, fieldName, value})
+)
--- app/components/Settings/index.jsx
+++ app/components/Settings/index.jsx
@@ -1,66 +1,99 @@
 /* @flow weak */
 import React, { Component } from 'react'
+import { bindActionCreator, bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import cx from 'classnames'
 import _ from 'lodash'
 
+import * as SettingsActions from './actions'
 
-let SettingState = {
-  activeTabId: 'EDITOR',
-  tabIds: ['GENERAL', 'EDITOR'],
-  tabs: {
-    'GENERAL': {
-      id: 'GENERAL',
-      language: {
-        value: 'english',
-        options: ['english', 'chinese']
-      },
-      theme: {
-        value: 'light',
-        options: ['light', 'dark']
+let FormGroupFactory = ({domain, settingItem, dispatch}) => {
+  let formComponent
+  let updateSettingItem = e => {
+    let value = (() => {
+      switch (e.target.type) {
+        case 'checkbox':
+          return e.target.checked
+        case 'number':
+          return Number(e.target.value)
+        case 'text':
+          return e.target.value
       }
-    },
-    'EDITOR': {
-      id: 'EDITOR',
-      items: [{
-        name: 'Keyboard Mode',
-        value: '',
-        options: ['default', 'vim', 'emacs']
-      }, {
-        name: 'Font Size',
-        value: 14
-      }, {
-        name: 'Font Family',
-        value: '',
-        options: ['Consolas', 'Courier', 'Courier New', 'Menlo']
-      }]
-    }
+    })()
+
+    return dispatch(
+      SettingsActions.updateSettingItem(domain, settingItem.name, value)
+    )
+  }
+
+  if (settingItem.options && _.isArray(settingItem.options)) {
+    return (
+      <div className='form-group'>
+        <label>{settingItem.name}</label>
+        <select className='form-control'
+          onChange={updateSettingItem}
+          value={settingItem.value} >
+          {settingItem.options.map(option =>
+            _.isObject(option) ?
+              <option key={option.value} value={option.value}>{option.name}</option>
+            : <option key={option} value={option}>{option}</option>
+          )}
+        </select>
+      </div>)
+  } else if (_.isBoolean(settingItem.value)) {
+    return (
+      <div className='form-group'>
+        <div className='checkbox'>
+          <label>
+            <input type='checkbox'
+              onChange={updateSettingItem} checked={settingItem.value} />
+            <strong>{settingItem.name}</strong>
+          </label>
+
+        </div>
+      </div>)
+  } else {
+    return (
+      <div className='form-group'>
+        <label>{settingItem.name}</label>
+        <input className='form-control'
+          type={_.isNumber(settingItem.value) ? 'number' : 'text'}
+          min='1'
+          onChange={updateSettingItem}
+          value={settingItem.value} />
+      </div>)
   }
 }
+FormGroupFactory = connect(null)(FormGroupFactory)
 
-const SettingsView = (_props) => {
-  const {activeTabId, tabIds, tabs} = SettingState
-
+const GeneralSettings = (props) => {
   return (
-    <div className='settings-container'>
-      <div className='settings-header'>
-        <div className='tab-bar-header'>Settings</div>
-        <ul className='tab-bar-tabs'>
-          {tabIds.map(tabId =>
-            <li key={tabId}
-              className={cx('tab-bar-item', {'active': tabId === activeTabId})}
-              onClick={e=>''} >{tabId}</li>
-          )}
-        </ul>
-      </div>
-      <div className='settings-content'>
-        <div className='settings-content-container'>
-          <SettingsContent content={tabs[activeTabId]} />
-        </div>
-      </div>
+    <div>
+      <h2 className='settings-content-header'>General Settings</h2>
+      {props.items.map(settingItem =>
+        <FormGroupFactory
+          key={settingItem.name}
+          domain='GENERAL'
+          settingItem={settingItem} />
+      )}
     </div>
   )
 }
+
+const EditorSettings = (props) => {
+  return (
+    <div>
+      <h2 className='settings-content-header'>Editor Settings</h2>
+      {props.items.map(settingItem =>
+        <FormGroupFactory
+          key={settingItem.name}
+          domain='EDITOR'
+          settingItem={settingItem} />
+      )}
+    </div>
+  )
+}
+
 
 const SettingsContent = ({content}) => {
   switch (content.id) {
@@ -72,78 +105,32 @@ const SettingsContent = ({content}) => {
   }
 }
 
-const FormGroupFactory = ({settingItem}) => {
-  let formComponent
-  if (settingItem.options && _.isArray(settingItem.options)) {
-    return (
-      <div className='form-group'>
-        <label>{settingItem.name}</label>
-        <select className="form-control"
-          onChange={e => console.log(e.target.value)}
-          value={settingItem.value} >
-          {settingItem.options.map(option =>
-            _.isObject(option) ?
-              <option key={option.value} value={option.value}>{option.name}</option>
-            : <option key={option} value={option}>{option}</option>
-          )}
-        </select>
-      </div>)
-  } else if (settingItem.isCheckbox) {
-    return (
-      <div className='form-group'>
-        <label>{settingItem.name}</label>
-        <input className="form-control"
-          type='checkbox'
-          value={settingItem.value} />
-      </div>)
-  } else {
-    return (
-      <div className='form-group'>
-        <label>{settingItem.name}</label>
-        <input className="form-control"
-          type='text'
-          value={settingItem.value} />
-      </div>)
-  }
-}
+let SettingsView = (props) => {
+  const {activeTabId, tabIds, tabs, activateSettingsTab} = props
 
-const GeneralSettings = ({language, theme}) => {
   return (
-    <div>
-      <h2 className='settings-content-header'>General Settings</h2>
-      <div className='form-group'>
-        <label>Language</label>
-        <select className="form-control"
-          onChange={e => ''}
-          value={language.value} >
-          {language.options.map( (item, i) =>
-            <option key={item} value={item}>{item}</option>
+    <div className='settings-container'>
+      <div className='settings-header'>
+        <div className='tab-bar-header'>Settings</div>
+        <ul className='tab-bar-tabs'>
+          {tabIds.map(tabId =>
+            <li key={tabId}
+              className={cx('tab-bar-item', {'active': tabId === activeTabId})}
+              onClick={e => activateSettingsTab(tabId)} >{tabId}</li>
           )}
-        </select>
+        </ul>
       </div>
-      <div className='form-group'>
-        <label>Theme</label>
-        <select className="form-control"
-          onChange={e => ''}
-          value={theme.value} >
-          {theme.options.map( (item, i) =>
-            <option key={item} value={item}>{item}</option>
-          )}
-        </select>
+      <div className='settings-content' style={{overflow:'scroll'}}>
+        <div className='settings-content-container'>
+          <SettingsContent content={tabs[activeTabId]} />
+        </div>
       </div>
     </div>
   )
 }
-
-const EditorSettings = (props) => {
-  return (
-    <div>
-      <h2 className='settings-content-header'>Editor Settings</h2>
-      {props.items.map(settingItem =>
-        <FormGroupFactory key={settingItem.name} settingItem={settingItem} />
-      )}
-    </div>
-  )
-}
+SettingsView = connect(
+  state => (state.SettingState),
+  dispatch => bindActionCreators(SettingsActions, dispatch)
+)(SettingsView)
 
 export default SettingsView
