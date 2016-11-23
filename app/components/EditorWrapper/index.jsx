b035e7c341e67ddc5f5133168c55b7cf45f3dc65
郑鑫祺 <candy.zhengxq@gmail.com>
2016-11-23 13:49:02 +0800

    add pic preview feature

--- app/components/EditorWrapper/index.jsx
+++ app/components/EditorWrapper/index.jsx
@@ -1,61 +1,73 @@
 import React, { PropTypes } from 'react';
 import AceEditor from '../AceEditor';
-import { markdown } from 'markdown';
-import { connect } from 'react-redux';
-import cx from 'classnames';
-import { bindActionCreators } from 'redux';
-import * as ResizeActions from './actions';
 import MarkdownEditor from '../MarkdownEditor';
+import PictureEditor from '../PictureEditor';
+
 
 
 const editors = {
   AceEditor,
   MarkdownEditor,
+  PictureEditor,
 };
 
 
 const getEditorByName = ({
-  type = 'default', 
-  tab, 
-  body, 
+  type = 'default',
+  tab,
+  body,
+  path
 }) => {
   if (type === 'default') {
     return React.createElement(editors.AceEditor, { tab });
-  } else if(type === 'editorWithPreview') {
+  } else if (type === 'editorWithPreview') {
     return React.createElement(editors.MarkdownEditor, { content: body, tab });
+  } else if (type === 'pictureEditor') {
+    return React.createElement(editors.PictureEditor, { path });
   }
+};
+
+const typeDetect = (title, types) => {
+  // title is the filename
+  // typeArray is the suffix
+  if (!Array.isArray(types)) return title.endsWith(`.${types}`);
+  return types.reduce((p, v) => p || title.endsWith(`.${v}`), false);
 };
 
 
 const EditorWrapper = ({
   tab,
-  actions
 }) => {
-  const { 
-    title, 
-    content: { body = '' } = {},
+  const {
+    title,
+    content: { body = '', path = '' } = {},
   } = tab;
   let type = 'default';
-  if (title.endsWith('.md')) {
-    type = 'editorWithPreview'
+  if (typeDetect(title, 'md')) {
+    type = 'editorWithPreview';
+  }
+  if (typeDetect(title, ['png', 'jpg', 'jpeg', 'gif'])) {
+    type = 'pictureEditor';
   }
   return getEditorByName({
     type,
     tab,
     body,
+    path
   });
 };
 
-EditorWrapper.PropTypes = {
+EditorWrapper.propTypes = {
   name: PropTypes.string,
   tab: PropTypes.object,
-}
+};
+
+getEditorByName.propTypes = {
+  type: PropTypes.string,
+  tab: PropTypes.object,
+  body: PropTypes.object,
+  path: PropTypes.string,
+};
+
 
 export default EditorWrapper;
-
-// export default connect(
-//   mapStateToProps,
-//   dispatch => ({
-//     actions: bindActionCreators(ResizeActions, dispatch)
-//   })
-// )(EditorWrapper)
