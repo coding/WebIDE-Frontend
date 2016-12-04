83c8c05cc258da39d2142b50840d721c7298959d
郑鑫祺 <candy.zhengxq@gmail.com>
2016-12-04 15:48:15 +0800

    add auto load required extensions

--- app/config.js
+++ app/config.js
@@ -1,6 +1,8 @@
 /* @flow weak */
 export default {
   projectName: '',
-  baseURL: '' || window.location.origin,
-  spaceKey: ''
+  baseURL: 'http://localhost:8080' || window.location.origin,
+  spaceKey: '',
+  extensionServer: 'http://localhost:8083' || process.env.EXTENSION_SERVER,
+  requiredExtensions: ['feature2', 'feature3']
 }
