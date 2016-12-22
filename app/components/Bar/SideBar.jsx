a4d378fb2fd0bf67df6e0fe70ad59140305612f6
hackape <muse.vincent@gmail.com>
2016-12-22 14:02:01 +0800

    nail the sidebar style

--- app/components/Bar/SideBar.jsx
+++ app/components/Bar/SideBar.jsx
@@ -1,10 +1,35 @@
+/* @flow weak */
+import { connect } from 'react-redux'
 import cx from 'classnames'
+import _ from 'lodash'
 
-export default ({ side }) => {
+const SideBarLabel = ({ label }) => {
   return (
-    <div className={'bar side-bar ' + side}>
-      this is side bar
+    <div className='side-bar-label'>
+      <div className='side-bar-label-container'>
+        <div className='side-bar-label-content'>
+          <i className={cx('icon', label.icon)}></i>
+          <span>{label.text}</span>
+        </div>
+      </div>
     </div>
   )
 }
 
+const _SideBar = ({ side, labels }) => {
+  return (
+    <div className={'bar side-bar ' + side}>
+      {labels.map(label => <SideBarLabel label={label} key={label.name}></SideBarLabel>)}
+    </div>
+  )
+}
+
+export default connect((state, { side }) => {
+  const localPackages = state.PackageState.localPackages
+  const labels = _.reduce(localPackages, (labels, pkg, pkgName) => {
+    if (pkg.type === 'extension' && pkg.ui.position === side)
+      labels.push({...pkg.ui.label, name: pkgName})
+    return labels
+  }, [])
+  return { labels }
+})(_SideBar)
