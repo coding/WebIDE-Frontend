2201dff4389c70d6cd4915fc5167d2aacab2ebb3
hackape <muse.vincent@gmail.com>
2017-03-02 16:37:54 +0800

    :sparkles: add `stepFactory` util for scenario of step-by-step process

---
+++ app/utils/stepFactory.js
@@ -0,0 +1,66 @@
+/**
+ * @param {function} descriptionHandler - will be passed the description message
+ * @param {function} successHandler - will be passed the success message / or the return value of success callback
+ * @param {function} errorHandler - will be passed the error message / or the return value of error callback
+ * @returns {function} step - step function, see below
+ */
+function stepFactory ({
+  descriptionHandler=console.log,
+  successHandler=console.log,
+  errorHandler=console.error,
+}={}) {
+  /**
+   * step function has the syntax of
+   * ```
+   * step(description, operation)
+   * .success(successMsg|successCallback)
+   * .error(errorMsg|errorCallback)
+   * ```
+   *
+   * step is best used with `await step(...)`
+   * it keeps a internal `allSuccess` state.
+   * if any step fails, subsequential steps won't be executed
+   *
+   * @param {OperationCallback} operation - the operation that you wanna take at this step.
+   *
+   * The operation callback:
+   * @callback OperationCallback
+   * @returns {Boolean|Promise}
+   * a boolean indicate the success/error state of this operation
+   * if it returns a promise, then the promise should resolve to boolean at final step
+   * if error is throw from the promise, it's seen as an error state
+   */
+  function step (description, fn) {
+    let successCallback, errorCallback
+    if (!step.allSuccess) return ({
+      success () { return this },
+      error () { return this }
+    })
+    descriptionHandler(description)
+    let promise = Promise.resolve(fn())
+    .then(isSuccess => {
+      if (!isSuccess) throw Error('error')
+      successCallback && successHandler(successCallback())
+    })
+    .catch(err => {
+      step.allSuccess = false
+      errorCallback && errorHandler(errorCallback())
+    })
+
+    promise.success = function (cb) {
+      successCallback = typeof cb === 'function' ? cb : (() => cb)
+      return this
+    }
+    promise.error = function (cb) {
+      errorCallback = typeof cb === 'function' ? cb : (() => cb)
+      return this
+    }
+
+    return promise
+  }
+
+  step.allSuccess = true
+  return step
+}
+
+export default stepFactory
