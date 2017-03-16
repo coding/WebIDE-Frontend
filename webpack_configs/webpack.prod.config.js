0e197e73bea2c064b21418efb5489bff45f5821d
hackape <muse.vincent@gmail.com>
2017-03-16 17:41:44 +0800

    update env variables

--- webpack_configs/webpack.prod.config.js
+++ webpack_configs/webpack.prod.config.js
@@ -15,8 +15,9 @@ module.exports = merge(
       new webpack.DefinePlugin({
         __DEV__: false,
         __RUN_MODE__: str(process.env.RUN_MODE || 'open'),
-        __BACKEND_URL__: str(process.env.BACKEND_URL || ''),
-        __WS_URL__: str(process.env.WS_URL || ''),
+        __BACKEND_URL__: str(process.env.RUN_MODE === 'platform' ? process.env.BACKEND_URL : 'http://localhost:8080'),
+        __WS_URL__: str(process.env.RUN_MODE === 'platform' ? process.env.WS_URL : ''),
+        __PACKAGE_SERVER__: str(process.env.PACKAGE_SERVER || process.env.HTML_BASE_URL),
       }),
     ]
   }
