cbd18e5546fa54c196cb6fb8a1627e589b0b193a
hackape <muse.vincent@gmail.com>
2017-04-12 16:48:57 +0800

    :construction: :fire: remove old Tab module

--- app/components/Panel/PanelsContainer.js
+++ app/components/Panel/PanelsContainer.js
@@ -2,13 +2,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import store from '../../store.js'
 import PanelAxis from './PanelAxis'
-import * as PanelActions from './actions'
-import PanesContainer from '../Pane'
-import TabContainer from '../Tab'
-import Terminal from '../Terminal'
-import FileTree from '../FileTree'
 
 const PrimaryPanelAxis = connect(state =>
   ({panel: state.PanelState.panels[state.PanelState.rootPanelId]})
--- app/components/Tab/index.js
+++
@@ -1,4 +0,0 @@
-export default from './TabContainer'
-export * as actions from './actions'
-export * as selectors from './selectors'
-export * as types from './types'
