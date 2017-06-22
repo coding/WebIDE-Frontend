738a1673b4ad06883a1725da52a334d7b2210245
hackape <muse.vincent@gmail.com>
2017-06-22 12:16:36 +0800

    fix some potential issues with utils/actions

--- app/components/Notification/actions.js
+++ app/components/Notification/actions.js
@@ -7,13 +7,13 @@ export const NOTIFY_TYPE = {
 }
 
 export const NOTIFICATION_REMOVE = 'NOTIFICATION_REMOVE'
-export const removeNotification = registerAction(NOTIFICATION_REMOVE, (notifKey) => {
+export const removeNotification = registerAction.normal(NOTIFICATION_REMOVE, (notifKey) => {
   const notifToRemove = state.notifications.find(notif => notif.key === notifKey)
   state.notifications.remove(notifToRemove)
 })
 
 const NOTIFICATION_ADD = 'NOTIFICATION_ADD'
-export const addNotification = registerAction(NOTIFICATION_ADD,
+export const addNotification = registerAction.normal(NOTIFICATION_ADD,
   (notification) => {
     const notifKey = Date.now()
     let defaultNotification = {
