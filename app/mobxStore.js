954e96dc818994fc96b21c711532d74ec0ddde6d
hackape <muse.vincent@gmail.com>
2017-07-20 14:32:04 +0800

    update mobxStore

--- app/mobxStore.js
+++ app/mobxStore.js
@@ -1,23 +1,33 @@
-import { autorun, createTransformer, toJS } from 'mobx'
+import { extendObservable, autorun, createTransformer, toJS } from 'mobx'
 import PaneState from './components/Pane/state'
 import EditorTabState from './components/Tab/state'
 import FileTreeState from './components/FileTree/state'
 import SettingState from './components/Setting/state'
+import FileState from './commons/File/state'
+
 const store = {
   PaneState,
   EditorTabState,
   FileTreeState,
   SettingState,
+  FileState,
 }
 
+extendObservable(store, {
+  debug: false,
+})
 const transform = createTransformer(store => ({
   PaneState: toJS(store.PaneState),
   EditorTabState: toJS(store.EditorTabState),
-  FileTreeState: toJS(store.FileTreeState)
+  FileTreeState: toJS(store.FileTreeState),
+  FileState: toJS(store.FileState)
 }))
-autorun((_) => {
-  const transformedStore = transform(store)
-  console.log('[mobx store] ', transformedStore)
+
+autorun(() => {
+  if (store.debug) {
+    const transformedStore = transform(store)
+    console.log('[mobx store] ', transformedStore)
+  }
 })
 
 export default store
