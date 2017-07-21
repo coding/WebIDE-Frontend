8149bfd03040b1be90c57e7efa05ed9a75945a2a
Yang Zhen <yangzhen@coding.net>
2017-07-21 15:46:48 +0800

    Add initialize page

---
+++ app/containers/Initialize/index.jsx
@@ -0,0 +1,35 @@
+import React, { Component, PropTypes } from 'react'
+import { observer } from 'mobx-react'
+import state from './state'
+
+@observer
+class Initialize extends Component {
+  render () {
+    let info = (
+      <div className='loading-info'>
+        Loading workspace...
+      </div>
+    )
+    if (state.errorInfo) {
+      info = (
+        <div className='loading-info error'>
+          Load workspace failed.
+        </div>
+      )
+    }
+
+    return (
+      <div className='initialize-container'>
+        <div className='coding-loading'></div>
+        {/* <div className='monkey splash-logo'></div> */}
+        {info}
+        {state.errorInfo && <div className='loading-error'>
+          <i className='fa fa-exclamation-triangle' />
+          {state.errorInfo}
+        </div>}
+      </div>
+    )
+  }
+}
+
+export default Initialize
