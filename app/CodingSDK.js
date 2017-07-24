4004056faa42272a13f09b8738aac66b1147ea5d
郑鑫祺 <candy.zhengxq@gmail.com>
2017-07-24 23:36:00 +0800

    重构 create18n，增加插件i18n支持

--- app/CodingSDK.js
+++ app/CodingSDK.js
@@ -1,16 +1,18 @@
 import { request } from './utils'
 import store from './store'
-import { bindActionCreators } from 'redux'
 import config from './config'
 import * as Modal from './components/Modal/actions'
 import { notify, NOTIFY_TYPE } from './components/Notification/actions'
 import { addComToSideBar } from './components/Panel/actions'
+import { CreateI18n } from 'utils/createI18n'
+
 
 export default class {
   // app data
   constructor (config = {}) {
     this.subscribeDataArray = config.subscribeDataArray || []
     this.pkgId = config.pkgId || ''
+    this.i18nConfig = config.i18n
   }
   getData () {
     const currentStore = store.getState()
@@ -33,6 +35,9 @@ export default class {
     return ({
       addComToSideBar
     })
+  }
+  get i18n () {
+    return new CreateI18n(this.i18nConfig || {})
   }
   get config () {
     return config
--- app/utils/createI18n.js
+++ app/utils/createI18n.js
@@ -12,53 +12,63 @@ const languageToCode = {
 }
 
 // support shape
-
-const replaceVariable = (translated, variables, formatFunc) => {
-  return translated.replace(/{(.+?)}/g, (variable) => {
-    const transfer = variables[variable.slice(1, -1)]
-    if (transfer) {
-      return formatFunc ? formatFunc(transfer) : transfer
-    }
-    return variable.slice(1, -1)
-  })
-}
-
-const translate = (origin = '', language, variableObj) => {
-  const dic = languageDicPool[language]
-  const key = origin.split(separator)[0]
-  if (!origin) {
-    return origin
-  }
-  // console.log('dic', dic, origin, _.get(dic, key))
-  if (!_.get(dic, key)) {
-    return origin.split(separator)[1] || origin
-  }
-  return replaceVariable(_.get(dic, key) || '', variableObj)
-}
-
 const mapStateToProps = () => {
   const languageSetting = settings.general.language
   const language = languageToCode[languageSetting.value]
   return ({ language })
 }
 
-
-const createI18n = (template = [], ...values) => {
-  const translateComponent = ({ language }) => {
-    const translatedWords = template.reduce((p, v, i) => `${p}${translate(v, language, values[i] || {})}`, '')
-    return (<span>{translatedWords}</span>)
+export class CreateI18n {
+  constructor ({ customLanguagePool = null }) {
+    this.languageDicPool = customLanguagePool || languageDicPool
+    this.i18nComponent = this.i18nComponent.bind(this)
+    this.translate = this.translate.bind(this)
+    this.replaceVariable = this.replaceVariable.bind(this)
+    this.getCache = this.getCache.bind(this)
   }
-  translateComponent.propTypes = {
-    language: PropTypes.string
+  translate (origin = '', language, variableObj) {
+    const dic = this.languageDicPool[language]
+    const key = origin.split(separator)[0]
+    if (!origin) {
+      return origin
+    }
+    // 当字典里找不到先看是否是开发模式下的:=，如果是就先暂时显示它，不是则原样返回
+    if (!_.get(dic, key)) {
+      return origin.split(separator)[1] || origin
+    }
+    return this.replaceVariable(_.get(dic, key) || '', variableObj)
   }
-  return React.createElement(inject(mapStateToProps)(translateComponent))
+  replaceVariable (translated, variables, formatFunc) {
+    return translated.replace(/{(.+?)}/g, (variable) => {
+      const transfer = variables[variable.slice(1, -1)]
+      if (transfer) {
+        return formatFunc ? formatFunc(transfer) : transfer
+      }
+      return variable.slice(1, -1)
+    })
+  }
+  i18nComponent (template = [], ...values) {
+    const translateComponent = ({ language }) => {
+      const translatedWords =
+      template.reduce((p, v, i) => `${p}${this.translate(v, language, values[i] || {})}`, '')
+      return (<span>{translatedWords}</span>)
+    }
+    translateComponent.propTypes = {
+      language: PropTypes.string
+    }
+    return React.createElement(inject(mapStateToProps)(translateComponent))
+  }
+  getCache (key, value) {
+    const language = languageToCode[settings.general.language.value]
+    return this.translate(key, language, value)
+  }
+  get language () {
+    return mapStateToProps().language
+  }
 }
 
 
-createI18n.get = (key, value) => {
-  const language = languageToCode[settings.general.language.value]
-  return translate(key, language, value)
-}
-
-export default createI18n
-
+const defaultInstance = new CreateI18n({})
+const i18n = defaultInstance.i18nComponent
+i18n.get = defaultInstance.getCache
+export default i18n
