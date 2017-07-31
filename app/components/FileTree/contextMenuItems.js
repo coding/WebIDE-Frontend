a549f3bedbbe4e1e4f6bee3a8d268597821b1caa
Yang Zhen <yangzhen@coding.net>
2017-07-31 17:27:30 +0800

    Fix dnd

--- app/components/FileTree/contextMenuItems.js
+++ app/components/FileTree/contextMenuItems.js
@@ -5,23 +5,28 @@ const items = [
   {
     name: i18n`fileTree.contextMenu.newFile`,
     icon: '',
-    command: 'file:new_file'
+    command: 'file:new_file',
+    id: 'filetree_menu_new_file',
   }, {
     name: i18n`fileTree.contextMenu.newFolder`,
     icon: '',
-    command: 'file:new_folder'
+    command: 'file:new_folder',
+    id: 'filetree_menu_new_folder',
   }, {
     name: i18n`fileTree.contextMenu.delete`,
     icon: '',
-    command: 'file:delete'
+    command: 'file:delete',
+    id: 'filetree_menu_delete',
   }, {
     name: i18n`fileTree.contextMenu.rename`,
     icon: '',
-    command: 'file:rename'
+    command: 'file:rename',
+    id: 'filetree_menu_rename',
   }, {
     name: i18n`fileTree.contextMenu.delete`,
     icon: '',
-    command: 'file:download'
+    command: 'file:download',
+    id: 'filetree_menu_download',
   }, {
     name: i18n`fileTree.contextMenu.upload`,
     icon: '',
@@ -31,13 +36,15 @@ const items = [
       const input = document.getElementById('filetree-hidden-input')
       input.dispatchEvent(new MouseEvent('click'))
     },
-    getIsHidden: ctx => !ctx.isDir
+    getIsHidden: ctx => !ctx.isDir,
+    id: 'filetree_menu_upload',
   }, {
     name: i18n`fileTree.contextMenu.gitBlame`,
     icon: '',
     command: (c) => {
       gitBlameNode(c)
     },
+    id: 'filetree_menu_gitBlame',
   }
 ]
 
