6aeadeb4bdf4029f28a369677ab4eeb7a9de936e
hackape <muse.vincent@gmail.com>
2017-08-03 16:59:17 +0800

    add a TopBar component as a wrapper around Breadcrumbs

--- app/components/exports.js
+++ app/components/exports.js
@@ -1,17 +1,19 @@
 import * as Accordion from './Accordion'
-import * as Breadcrumbs from './Breadcrumbs'
+import * as TopBar from './TopBar'
 import * as ContextMenu from './ContextMenu'
 import * as Editor from './Editor'
 import * as Menu from './Menu'
 import * as Modal from './Modal'
+import * as Plugins from './Plugins'
 import * as Tab from './Tab'
 
 export {
   Accordion,
-  Breadcrumbs,
+  TopBar,
   ContextMenu,
   Editor,
   Menu,
   Modal,
+  Plugins,
   Tab,
 }
