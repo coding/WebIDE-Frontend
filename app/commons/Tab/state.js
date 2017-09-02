7894e92e50bcbaf923bfdf09d6f41da4fc5bf2b1
hackape <muse.vincent@gmail.com>
2017-09-02 20:26:30 +0800

    re-enable autorun that maintains tab indexes

--- app/commons/Tab/state.js
+++ app/commons/Tab/state.js
@@ -77,15 +77,14 @@ function TabScope () {
   }
 }
 
-  // autorun(() => {
-  //   state.tabGroups.forEach((tabGroup) => {
-  //   // correct tab index
-  //     tabGroup.tabs.forEach((tab, tabIndex) => {
-  //       if (tab.index !== tabIndex) tab.index = tabIndex
-  //     })
-  //   })
-  // })
-
+  autorun(() => {
+    state.tabGroups.forEach((tabGroup) => {
+    // correct tab index
+      tabGroup.tabs.forEach((tab, tabIndex) => {
+        if (tab.index !== tabIndex) tab.index = tabIndex
+      })
+    })
+  })
 
   class TabGroup {
     static Tab = Tab;
