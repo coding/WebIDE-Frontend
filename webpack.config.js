faf17d6973606f16d9cac7d2dbfa05f1e1deeddb
郑鑫祺 <candy.zhengxq@gmail.com>
2017-11-01 17:12:25 +0800

    增加 staging 状态下的 webpack 配置，去除uglify，增加的 devtool支持

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+/*eslint-disable*/
 require('dotenv').config()
-module.exports = process.env.NODE_ENV === 'production' ?
+module.exports = (process.env.NODE_ENV === 'production') || (!process.env.NODE_ENV) ?
     require('./webpack_configs/webpack.prod.config.js')
-  : require('./webpack_configs/webpack.dev.config.js')
+  : (process.env.NODE_ENV === 'staging' ? require('./webpack_configs/webpack.staging.config.js') :
+  require('./webpack_configs/webpack.dev.config.js'))
---
+++ webpack_configs/webpack.staging.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+
+const str = JSON.stringify
+const commonConfig = require('./common.config.js')
+const stylesheet = require('./stylesheet.config')
+// const uglify = require('./uglify.config')
+
+module.exports = merge(
+  commonConfig({
+    staticDir: process.env.RUN_MODE ? 'rs2' : 'rs',
+  }),
+  stylesheet(),
+  { devtool: 'cheap-module-eval-source-map' },
+  {
+    plugins: [
+      new webpack.DefinePlugin({
+        __DEV__: false,
+        __RUN_MODE__: str(process.env.RUN_MODE || ''),
+        __BACKEND_URL__: str(process.env.BACKEND_URL || ''),
+        __WS_URL__: str(process.env.WS_URL || ''),
+        __PACKAGE_DEV__: false,
+        __PACKAGE_SERVER__: str(process.env.PACKAGE_SERVER || process.env.HTML_BASE_URL || ''),
+      }),
+    ]
+  }
+)
