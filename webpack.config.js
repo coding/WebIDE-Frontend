42701b798a151869921e60804659dd7d58aaf831
Yang Zhen <yangzhen@coding.net>
2017-11-07 14:24:47 +0800

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 require('dotenv').config()
 var production = ['production', 'prod'];
 
-module.exports = (production.indexOf(process.env.NODE_ENV) > -1) || (!process.env.NODE_ENV) ?
+module.exports = (process.env.NODE_ENV && production.indexOf(process.env.NODE_ENV) > -1) ?
     require('./webpack_configs/webpack.prod.config.js')
   : (process.env.NODE_ENV === 'staging' ? require('./webpack_configs/webpack.staging.config.js') :
   require('./webpack_configs/webpack.dev.config.js'))
