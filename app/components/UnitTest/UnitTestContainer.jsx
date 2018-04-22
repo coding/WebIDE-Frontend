b4080d9f536ea6474dbe4f1ceb9ce61af55534f8
Yang Zhen <yangzhen@coding.net>
2018-04-22 13:40:03 +0800

    Fix error when output state is blank

--- app/components/UnitTest/UnitTestContainer.jsx
+++ app/components/UnitTest/UnitTestContainer.jsx
@@ -5,13 +5,18 @@ import state from './state'
 @observer
 class UnitTestContainer extends Component {
   render () {
-    return (
-      <div className='unit-test-output'>
-        <div className='sum'>
-          错误 {state.testOutput.count.value} | 跳过 {state.testOutput.ignoreCount.value} | 运行时间 {state.testOutput.runTime.value}
+    if (state.testOutput) {
+      return (
+        <div className='unit-test-output'>
+          <div className='sum'>
+            错误 {state.testOutput.count.value} | 跳过 {state.testOutput.ignoreCount.value} | 运行时间 {state.testOutput.runTime.value}
+          </div>
+          {JSON.stringify(state.testOutput.failures)}
         </div>
-        {JSON.stringify(state.testOutput.failures)}
-      </div>
+      )
+    }
+    return (
+      <div className='unit-test-output'></div>
     )
   }
 }
