461b794108b7e9d9a04c7b2bbf42d1de14f9d59d
Yang Zhen <yangzhen@coding.net>
2018-04-22 13:27:56 +0800

    Fix output of run test

--- app/components/UnitTest/UnitTestContainer.jsx
+++ app/components/UnitTest/UnitTestContainer.jsx
@@ -6,7 +6,12 @@ import state from './state'
 class UnitTestContainer extends Component {
   render () {
     return (
-      <div className='unit-test-output'>{state.testOutput}</div>
+      <div className='unit-test-output'>
+        <div className='sum'>
+          错误 {state.testOutput.count.value} | 跳过 {state.testOutput.ignoreCount.value} | 运行时间 {state.testOutput.runTime.value}
+        </div>
+        {JSON.stringify(state.testOutput.failures)}
+      </div>
     )
   }
 }
