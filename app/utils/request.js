239ac4110dd3032cedde0efa5b9b64ad14cd3028
Yang Zhen <yangzhen@coding.net>
2018-05-25 15:20:38 +0800

    Use file saver for file download

--- app/utils/request.js
+++ app/utils/request.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import FileSaver from 'file-saver'
 import './promise.prototype.finalCatch'
 import qs from './qs'
 import config from '../config'
@@ -157,32 +158,7 @@ request.download = function (url, filename, params, options = {}) {
     params,
     ...options,
   }).then((response) => {
-    if (typeof window.navigator.msSaveBlob !== 'undefined') {
-      // IE workaround for "HTML7007: One or more blob URLs were 
-      // revoked by closing the blob for which they were created. 
-      // These URLs will no longer resolve as the data backing 
-      // the URL has been freed."
-      window.navigator.msSaveBlob(response, filename)
-    } else {
-      const blobURL = window.URL.createObjectURL(response)
-      const tempLink = document.createElement('a')
-      tempLink.style.display = 'none'
-      tempLink.href = blobURL
-      tempLink.setAttribute('download', filename)
-      
-      // Safari thinks _blank anchor are pop ups. We only want to set _blank
-      // target if the browser does not support the HTML5 download attribute.
-      // This allows you to download files in desktop safari if pop up blocking 
-      // is enabled.
-      if (typeof tempLink.download === 'undefined') {
-        tempLink.setAttribute('target', '_blank')
-      }
-    
-      document.body.appendChild(tempLink)
-      tempLink.click()
-      document.body.removeChild(tempLink)
-      window.URL.revokeObjectURL(blobURL)
-    }
+    FileSaver.saveAs(response, filename)
   })
 }
 
