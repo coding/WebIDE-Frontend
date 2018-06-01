89b77daa0473470314762ca92711a5a5363df98b
sakuraash <841105127@qq.com>
2018-06-01 18:40:47 +0800

    update config

--- webpack_configs/monaco-editor-webpack-plugin/index.js
+++ webpack_configs/monaco-editor-webpack-plugin/index.js
@@ -1,103 +1,271 @@
-const path = require('path');
-const webpack = require('webpack');
-const AddWorkerEntryPointPlugin = require('./plugins/AddWorkerEntryPointPlugin');
-const INCLUDE_LOADER_PATH = require.resolve('./loaders/include');
+"use strict";
 
-const IGNORED_IMPORTS = {
-  [resolveMonacoPath('vs/language/typescript/lib/typescriptServices')]: [
-    'fs',
-    'path',
-    'os',
-    'crypto',
-    'source-map-support',
-  ],
-};
-const MONACO_EDITOR_API_PATHS = [
-  resolveMonacoPath('vs/editor/editor.main'),
-  resolveMonacoPath('vs/editor/editor.api')
-];
-const WORKER_LOADER_PATH = resolveMonacoPath('vs/editor/common/services/editorSimpleWorker');
-const EDITOR_MODULE = {
-  label: 'editorWorkerService',
-  entry: undefined,
-  worker: {
-    id: 'vs/editor/editor',
-    entry: 'vs/editor/editor.worker',
-    output: 'editor.worker.js',
-    fallback: undefined
-  },
-  alias: undefined,
-};
-const LANGUAGES = require('./languages');
-const FEATURES = require('./features');
+var _createClass = (function() {
+  function defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }
+  return function(Constructor, protoProps, staticProps) {
+    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) defineProperties(Constructor, staticProps);
+    return Constructor;
+  };
+})();
 
-function resolveMonacoPath(filePath) {
-  return require.resolve(path.join('monaco-editor/esm', filePath));
+var _extends =
+  Object.assign ||
+  function(target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+    return target;
+  };
+
+var _slicedToArray = (function() {
+  function sliceIterator(arr, i) {
+    var _arr = [];
+    var _n = true;
+    var _d = false;
+    var _e = undefined;
+    try {
+      for (
+        var _i = arr[Symbol.iterator](), _s;
+        !(_n = (_s = _i.next()).done);
+        _n = true
+      ) {
+        _arr.push(_s.value);
+        if (i && _arr.length === i) break;
+      }
+    } catch (err) {
+      _d = true;
+      _e = err;
+    } finally {
+      try {
+        if (!_n && _i["return"]) _i["return"]();
+      } finally {
+        if (_d) throw _e;
+      }
+    }
+    return _arr;
+  }
+  return function(arr, i) {
+    if (Array.isArray(arr)) {
+      return arr;
+    } else if (Symbol.iterator in Object(arr)) {
+      return sliceIterator(arr, i);
+    } else {
+      throw new TypeError(
+        "Invalid attempt to destructure non-iterable instance"
+      );
+    }
+  };
+})();
+
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
 }
 
-const languagesById = fromPairs(
-  flatMap(toPairs(LANGUAGES), ([id, language]) =>
-    [id, ...(language.alias || [])].map((label) => [label, { label, ...language }])
-  )
-);
-const featuresById = mapValues(FEATURES, (feature, key) => ({ label: key, ...feature }))
+function _toConsumableArray(arr) {
+  if (Array.isArray(arr)) {
+    for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
+      arr2[i] = arr[i];
+    }
+    return arr2;
+  } else {
+    return Array.from(arr);
+  }
+}
 
-class MonacoWebpackPlugin {
-  constructor(options = {}) {
-    const languages = options.languages || Object.keys(languagesById);
-    const features = options.features || Object.keys(featuresById);
-    const output = options.output || '';
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    });
+  } else {
+    obj[key] = value;
+  }
+  return obj;
+}
+
+var path = require("path");
+var webpack = require("webpack");
+var AddWorkerEntryPointPlugin = require("./plugins/AddWorkerEntryPointPlugin");
+var INCLUDE_LOADER_PATH = require.resolve("./loaders/include");
+
+var IGNORED_IMPORTS = _defineProperty(
+  {},
+  resolveMonacoPath("vs/language/typescript/lib/typescriptServices"),
+  ["fs", "path", "os", "crypto", "source-map-support"]
+);
+var MONACO_EDITOR_API_PATHS = [
+  resolveMonacoPath("vs/editor/editor.main"),
+  resolveMonacoPath("vs/editor/editor.api")
+];
+var WORKER_LOADER_PATH = resolveMonacoPath(
+  "vs/editor/common/services/editorSimpleWorker"
+);
+var EDITOR_MODULE = {
+  label: "editorWorkerService",
+  entry: undefined,
+  worker: {
+    id: "vs/editor/editor",
+    entry: "vs/editor/editor.worker",
+    output: "editor.worker.js",
+    fallback: undefined
+  },
+  alias: undefined
+};
+var LANGUAGES = require("./languages");
+var FEATURES = require("./features");
+
+function resolveMonacoPath(filePath) {
+  return require.resolve(path.join("monaco-editor/esm", filePath));
+}
+
+var languagesById = fromPairs(
+  flatMap(toPairs(LANGUAGES), function(_ref) {
+    var _ref2 = _slicedToArray(_ref, 2),
+      id = _ref2[0],
+      language = _ref2[1];
+
+    return [id]
+      .concat(_toConsumableArray(language.alias || []))
+      .map(function(label) {
+        return [label, _extends({ label: label }, language)];
+      });
+  })
+);
+var featuresById = mapValues(FEATURES, function(feature, key) {
+  return _extends({ label: key }, feature);
+});
+
+var MonacoWebpackPlugin = (function() {
+  function MonacoWebpackPlugin() {
+    var options =
+      arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+    _classCallCheck(this, MonacoWebpackPlugin);
+
+    var languages = options.languages || Object.keys(languagesById);
+    var features = options.features || Object.keys(featuresById);
+    var output = options.output || "";
     this.options = {
-      languages: languages.map((id) => languagesById[id]).filter(Boolean),
-      features: features.map(id => featuresById[id]).filter(Boolean),
-      output,
+      languages: languages
+        .map(function(id) {
+          return languagesById[id];
+        })
+        .filter(Boolean),
+      features: features
+        .map(function(id) {
+          return featuresById[id];
+        })
+        .filter(Boolean),
+      output: output
     };
   }
 
-  apply(compiler) {
-    const { languages, features, output } = this.options;
-    const publicPath = getPublicPath(compiler);
-    const modules = [EDITOR_MODULE, ...languages, ...features];
-    const workers = modules.map(
-      ({ label, alias, worker }) => worker && ({ label, alias, ...worker })
-    ).filter(Boolean);
-    const rules = createLoaderRules(languages, features, workers, publicPath);
-    const plugins = createPlugins(workers, output);
-    addCompilerRules(compiler, rules);
-    addCompilerPlugins(compiler, plugins);
-  }
-}
+  _createClass(MonacoWebpackPlugin, [
+    {
+      key: "apply",
+      value: function apply(compiler) {
+        var _options = this.options,
+          languages = _options.languages,
+          features = _options.features,
+          output = _options.output;
+
+        var publicPath = getPublicPath(compiler);
+        var modules = [EDITOR_MODULE].concat(
+          _toConsumableArray(languages),
+          _toConsumableArray(features)
+        );
+        var workers = modules
+          .map(function(_ref3) {
+            var label = _ref3.label,
+              alias = _ref3.alias,
+              worker = _ref3.worker;
+            return worker && _extends({ label: label, alias: alias }, worker);
+          })
+          .filter(Boolean);
+        var rules = createLoaderRules(languages, features, workers, publicPath);
+        var plugins = createPlugins(workers, output);
+        addCompilerRules(compiler, rules);
+        addCompilerPlugins(compiler, plugins);
+      }
+    }
+  ]);
+
+  return MonacoWebpackPlugin;
+})();
 
 function addCompilerRules(compiler, rules) {
-  const compilerOptions = compiler.options;
-  const moduleOptions = compilerOptions.module || (compilerOptions.module = {});
-  const existingRules = moduleOptions.rules || (moduleOptions.rules = []);
-  existingRules.push(...rules);
+  var compilerOptions = compiler.options;
+  var moduleOptions = compilerOptions.module || (compilerOptions.module = {});
+  var existingRules = moduleOptions.rules || (moduleOptions.rules = []);
+  existingRules.push.apply(existingRules, _toConsumableArray(rules));
 }
 
 function addCompilerPlugins(compiler, plugins) {
-  plugins.forEach((plugin) => plugin.apply(compiler));
+  plugins.forEach(function(plugin) {
+    return plugin.apply(compiler);
+  });
 }
 
 function getPublicPath(compiler) {
-  return compiler.options.output && compiler.options.output.publicPath || '';
+  return (compiler.options.output && compiler.options.output.publicPath) || "";
 }
 
 function stripTrailingSlash(string) {
-  return string.replace(/\/$/, '');
+  return string.replace(/\/$/, "");
 }
 
 function createLoaderRules(languages, features, workers, publicPath) {
-  if (!languages.length && !features.length) { return []; }
-  const languagePaths = languages.map(({ entry }) => entry).filter(Boolean);
-  const featurePaths = features.map(({ entry }) => entry).filter(Boolean);
-  const workerPaths = workers.reduce((acc, { label, output }) => Object.assign(acc, {
-    [label]: `${publicPath ? `${stripTrailingSlash(publicPath)}/` : ''}${output}`,
-  }), {});
-  const globals = {
-    'MonacoEnvironment': `((paths) => ({ getWorkerUrl: (moduleId, label) => paths[label] }))(${
-      JSON.stringify(workerPaths, null, 2)
-    })`,
+  if (!languages.length && !features.length) {
+    return [];
+  }
+  var languagePaths = languages
+    .map(function(_ref4) {
+      var entry = _ref4.entry;
+      return entry;
+    })
+    .filter(Boolean);
+  var featurePaths = features
+    .map(function(_ref5) {
+      var entry = _ref5.entry;
+      return entry;
+    })
+    .filter(Boolean);
+  var workerPaths = workers.reduce(function(acc, _ref6) {
+    var label = _ref6.label,
+      output = _ref6.output;
+    return Object.assign(
+      acc,
+      _defineProperty(
+        {},
+        label,
+        "" + (publicPath ? stripTrailingSlash(publicPath) + "/" : "") + output
+      )
+    );
+  }, {});
+  var globals = {
+    MonacoEnvironment:
+      "((paths) => ({ getWorkerUrl: (moduleId, label) => paths[label] }))(" +
+      JSON.stringify(workerPaths, null, 2) +
+      ")"
   };
   return [
     {
@@ -106,78 +274,122 @@ function createLoaderRules(languages, features, workers, publicPath) {
         {
           loader: INCLUDE_LOADER_PATH,
           options: {
-            globals,
-            pre: featurePaths.map((importPath) => resolveMonacoPath(importPath)),
-            post: languagePaths.map((importPath) => resolveMonacoPath(importPath)),
-          },
-        },
-      ],
-    },
+            globals: globals,
+            pre: featurePaths.map(function(importPath) {
+              return resolveMonacoPath(importPath);
+            }),
+            post: languagePaths.map(function(importPath) {
+              return resolveMonacoPath(importPath);
+            })
+          }
+        }
+      ]
+    }
   ];
 }
 
 function createPlugins(workers, outputPath) {
-  const workerFallbacks = workers.reduce((acc, { id, fallback }) => (fallback ? Object.assign(acc, {
-    [id]: resolveMonacoPath(fallback)
-  }) : acc), {});
-  return [
-    ...Object.keys(IGNORED_IMPORTS).map((id) =>
-      createIgnoreImportsPlugin(id, IGNORED_IMPORTS[id])
-    ),
-    ...uniqBy(workers, ({ id }) => id).map(({ id, entry, output }) =>
-      new AddWorkerEntryPointPlugin({
-        id,
-        entry: resolveMonacoPath(entry),
-        filename: path.join(outputPath, output),
-        plugins: [
-          createContextPlugin(WORKER_LOADER_PATH, {}),
-          new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
-        ],
+  var workerFallbacks = workers.reduce(function(acc, _ref7) {
+    var id = _ref7.id,
+      fallback = _ref7.fallback;
+    return fallback
+      ? Object.assign(acc, _defineProperty({}, id, resolveMonacoPath(fallback)))
+      : acc;
+  }, {});
+  return [].concat(
+    _toConsumableArray(
+      Object.keys(IGNORED_IMPORTS).map(function(id) {
+        return createIgnoreImportsPlugin(id, IGNORED_IMPORTS[id]);
       })
     ),
-    ...(workerFallbacks ? [createContextPlugin(WORKER_LOADER_PATH, workerFallbacks)] : []),
-  ];
+    _toConsumableArray(
+      uniqBy(workers, function(_ref8) {
+        var id = _ref8.id;
+        return id;
+      }).map(function(_ref9) {
+        var id = _ref9.id,
+          entry = _ref9.entry,
+          output = _ref9.output;
+        return new AddWorkerEntryPointPlugin({
+          id: id,
+          entry: resolveMonacoPath(entry),
+          filename: path.join(outputPath, output),
+          plugins: [
+            createContextPlugin(WORKER_LOADER_PATH, {}),
+            new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 })
+          ]
+        });
+      })
+    ),
+    _toConsumableArray(
+      workerFallbacks
+        ? [createContextPlugin(WORKER_LOADER_PATH, workerFallbacks)]
+        : []
+    )
+  );
 }
 
 function createContextPlugin(filePath, contextPaths) {
   return new webpack.ContextReplacementPlugin(
-    new RegExp(`^${path.dirname(filePath)}$`),
-    '',
+    new RegExp("^" + path.dirname(filePath) + "$"),
+    "",
     contextPaths
   );
 }
 
 function createIgnoreImportsPlugin(targetPath, ignoredModules) {
   return new webpack.IgnorePlugin(
-    new RegExp(`^(${ignoredModules.map((id) => `(${id})`).join('|')})$`),
-    new RegExp(`^${path.dirname(targetPath)}$`)
+    new RegExp(
+      "^(" +
+        ignoredModules
+          .map(function(id) {
+            return "(" + id + ")";
+          })
+          .join("|") +
+        ")$"
+    ),
+    new RegExp("^" + path.dirname(targetPath) + "$")
   );
 }
 
 function flatMap(items, iteratee) {
-  return items.map(iteratee).reduce((acc, item) => [...acc, ...item], []);
+  return items.map(iteratee).reduce(function(acc, item) {
+    return [].concat(_toConsumableArray(acc), _toConsumableArray(item));
+  }, []);
 }
 
 function toPairs(object) {
-  return Object.keys(object).map((key) => [key, object[key]]);
+  return Object.keys(object).map(function(key) {
+    return [key, object[key]];
+  });
 }
 
 function fromPairs(values) {
-  return values.reduce((acc, [key, value]) => Object.assign(acc, { [key]: value }), {});
+  return values.reduce(function(acc, _ref10) {
+    var _ref11 = _slicedToArray(_ref10, 2),
+      key = _ref11[0],
+      value = _ref11[1];
+
+    return Object.assign(acc, _defineProperty({}, key, value));
+  }, {});
 }
 
 function mapValues(object, iteratee) {
-  return Object.keys(object).reduce(
-    (acc, key) => Object.assign(acc, { [key]: iteratee(object[key], key) }),
-    {}
-  );
+  return Object.keys(object).reduce(function(acc, key) {
+    return Object.assign(
+      acc,
+      _defineProperty({}, key, iteratee(object[key], key))
+    );
+  }, {});
 }
 
 function uniqBy(items, iteratee) {
-  const keys = {};
-  return items.reduce((acc, item) => {
-    const key = iteratee(item);
-    if (key in keys) { return acc; }
+  var keys = {};
+  return items.reduce(function(acc, item) {
+    var key = iteratee(item);
+    if (key in keys) {
+      return acc;
+    }
     keys[key] = true;
     acc.push(item);
     return acc;
--- webpack_configs/monaco-editor-webpack-plugin/plugins/AddWorkerEntryPointPlugin.js
+++ webpack_configs/monaco-editor-webpack-plugin/plugins/AddWorkerEntryPointPlugin.js
@@ -1,37 +1,92 @@
-const singleEntry = require('webpack/lib/SingleEntryPlugin');
-const loaderTarget = require('webpack/lib/LoaderTargetPlugin');
-const webworker = require('webpack/lib/webworker/WebWorkerTemplatePlugin');
+"use strict";
 
-class AddWorkerEntryPointPlugin {
-  constructor({
-    id,
-    entry,
-    filename,
-    chunkFilename = undefined,
-    plugins = undefined,
-  }) {
-    this.options = { id, entry, filename, chunkFilename, plugins };
+var _createClass = (function() {
+  function defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
   }
+  return function(Constructor, protoProps, staticProps) {
+    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) defineProperties(Constructor, staticProps);
+    return Constructor;
+  };
+})();
 
-  apply(compiler) {
-    const { id, entry, filename, chunkFilename, plugins } = this.options;
-    compiler.plugin('make', (compilation, callback) => {
-      const outputOptions = {
-        filename,
-        chunkFilename,
-        publicPath: compilation.outputOptions.publicPath,
-        // HACK: globalObject is necessary to fix https://github.com/webpack/webpack/issues/6642
-        globalObject: 'this',
-      };
-      const childCompiler = compilation.createChildCompiler(id, outputOptions, [
-        new webworker(),
-        new loaderTarget('webworker'),
-        new singleEntry(compiler.context, entry, 'main'),
-      ]);
-      plugins.forEach((plugin) => plugin.apply(childCompiler));
-      childCompiler.runAsChild(callback);
-    });
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
   }
 }
+
+var singleEntry = require("webpack/lib/SingleEntryPlugin");
+var loaderTarget = require("webpack/lib/LoaderTargetPlugin");
+var webworker = require("webpack/lib/webworker/WebWorkerTemplatePlugin");
+
+var AddWorkerEntryPointPlugin = (function() {
+  function AddWorkerEntryPointPlugin(_ref) {
+    var id = _ref.id,
+      entry = _ref.entry,
+      filename = _ref.filename,
+      _ref$chunkFilename = _ref.chunkFilename,
+      chunkFilename =
+        _ref$chunkFilename === undefined ? undefined : _ref$chunkFilename,
+      _ref$plugins = _ref.plugins,
+      plugins = _ref$plugins === undefined ? undefined : _ref$plugins;
+
+    _classCallCheck(this, AddWorkerEntryPointPlugin);
+
+    this.options = {
+      id: id,
+      entry: entry,
+      filename: filename,
+      chunkFilename: chunkFilename,
+      plugins: plugins
+    };
+  }
+
+  _createClass(AddWorkerEntryPointPlugin, [
+    {
+      key: "apply",
+      value: function apply(compiler) {
+        var _options = this.options,
+          id = _options.id,
+          entry = _options.entry,
+          filename = _options.filename,
+          chunkFilename = _options.chunkFilename,
+          plugins = _options.plugins;
+
+        compiler.plugin("make", function(compilation, callback) {
+          var outputOptions = {
+            filename: filename,
+            chunkFilename: chunkFilename,
+            publicPath: compilation.outputOptions.publicPath,
+            // HACK: globalObject is necessary to fix https://github.com/webpack/webpack/issues/6642
+            globalObject: "this"
+          };
+          var childCompiler = compilation.createChildCompiler(
+            id,
+            outputOptions,
+            [
+              new webworker(),
+              new loaderTarget("webworker"),
+              new singleEntry(compiler.context, entry, "main")
+            ]
+          );
+          plugins.forEach(function(plugin) {
+            return plugin.apply(childCompiler);
+          });
+          childCompiler.runAsChild(callback);
+        });
+      }
+    }
+  ]);
+
+  return AddWorkerEntryPointPlugin;
+})();
 
 module.exports = AddWorkerEntryPointPlugin;
