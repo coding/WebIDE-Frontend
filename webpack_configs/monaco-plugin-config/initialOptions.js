3896dbba3a627d9a394ca573e547a69d7d60ac94
sakuraash <841105127@qq.com>
2018-07-05 14:13:28 +0800

    update monaco config

--- webpack_configs/monaco-plugin-config/initialOptions.js
+++ webpack_configs/monaco-plugin-config/initialOptions.js
@@ -1,4 +1,4 @@
 module.exports = {
-  languages: ['typescript', 'json', 'html', 'css'],
+  languages: ['bat', 'coffee', 'cpp', 'csharp', 'csp', 'css', 'dockerfile', 'fsharp', 'go', 'handlebars', 'html', 'ini', 'java', 'json', 'less', 'lua', 'markdown', 'msdax', 'mysql', 'objective', 'pgsql', 'php', 'postiats', 'powershell', 'pug', 'python', 'r', 'razor', 'redis', 'redshift', 'ruby', 'sb', 'scss', 'solidity', 'sql', 'swift', 'typescript', 'vb', 'xml', 'yaml'],
   features: ['']
 }
