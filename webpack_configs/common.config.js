0407aa67a2d4e9a3048bbbce56a3c3a0b0a89a9b
sakuraash <841105127@qq.com>
2018-07-05 17:25:18 +0800

    update config

--- webpack_configs/common.config.js
+++ webpack_configs/common.config.js
@@ -9,7 +9,6 @@ const gitRevisionPlugin = new GitRevisionPlugin()
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 const initMonacoPluginConfig = require('./monaco-plugin-config/initialOptions')
 const HappyPack = require('happypack')
-
 const os = require('os')
 
 const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length })
@@ -17,7 +16,7 @@ const PROJECT_ROOT = path.resolve(__dirname, '..')
 // const ICO_PATH = path.join(PROJECT_ROOT, 'static/favicon.ico')
 // const ICO_PATH = '//studio-res.coding.net/StudioWebResource/Images/favicon.ico'
 
-module.exports = function (options={}) {
+module.exports = function(options = {}) {
   const {
     mainEntryHtmlName = 'workspace.html',
     accountEntryHtmlName = 'account.html',
@@ -31,68 +30,98 @@ module.exports = function (options={}) {
     chunkFilename = '[name].[hash].chunk.js'
   } = options
 
-  const publicPath = process.env.QINIU_BUCKET ? // publicPath should end with '/'
-    `${process.env.QINIU_SERVER}/` : path.join('/', staticDir, '/')
-return {
-  entry: {
-    main: [path.join(PROJECT_ROOT, 'app')],
-    workspaces: [path.join(PROJECT_ROOT, 'app/workspaces_standalone')],
-    login: [path.join(PROJECT_ROOT, 'app/login.jsx')],
-    // intro: [path.join(PROJECT_ROOT, 'app/intro.jsx')],
-    vendor: ['@babel/polyfill', 'react', 'react-dom', 'redux', 'react-redux'],
-  },
-  output: {
-    publicPath,
-    path: path.join(PROJECT_ROOT, 'build', staticDir),
-    filename,
-    chunkFilename,
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx'],
-    modules: ['node_modules', path.join(PROJECT_ROOT, 'app')],
-    alias: {
-      static: path.join(PROJECT_ROOT, 'static'),
+  const publicPath = process.env.QINIU_BUCKET // publicPath should end with '/'
+    ? `${process.env.QINIU_SERVER}/`
+    : path.join('/', staticDir, '/')
+  return {
+    entry: {
+      main: [path.join(PROJECT_ROOT, 'app')],
+      workspaces: [path.join(PROJECT_ROOT, 'app/workspaces_standalone')],
+      login: [path.join(PROJECT_ROOT, 'app/login.jsx')],
+      // intro: [path.join(PROJECT_ROOT, 'app/intro.jsx')],
+      babel: ['@babel/polyfill'],
+      vendor: ['react', 'react-dom', 'redux', 'react-redux']
+    },
+    output: {
+      publicPath,
+      path: path.join(PROJECT_ROOT, 'build', staticDir),
+      filename,
+      chunkFilename
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx'],
+      modules: ['node_modules', path.join(PROJECT_ROOT, 'app')],
+      alias: {
+        static: path.join(PROJECT_ROOT, 'static')
+      }
+    },
+    resolveLoader: {
+      modules: [path.resolve(__dirname, './loaders/'), 'node_modules']
+    },
+    node: {
+      fs: 'empty',
+      child_process: 'empty',
+      net: 'empty',
+      crypto: 'empty'
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          commons: {
+            name: 'commons',
+            chunks: 'initial',
+            minChunks: 2
+          },
+          vendors: {
+            chunks: 'initial',
+            name: 'vendors',
+            test: /node_modules\//,
+            minChunks: 5,
+            priority: 10,
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true
+          }
+        }
+      }
+    },
+    plugins: [
+      gitRevisionPlugin,
+      // new BundleAnalyzerPlugin(),
+      new DefinePlugin({
+        __VERSION__: str(gitRevisionPlugin.commithash() + '@' + gitRevisionPlugin.version()),
+        __PUBLIC_PATH__: str(publicPath)
+      }),
+      // https://github.com/kevlened/copy-webpack-plugin
+      new CopyWebpackPlugin([
+        {
+          from: path.join(PROJECT_ROOT, 'static')
+        },
+        {
+          from: path.join(PROJECT_ROOT, 'node_modules/font-awesome'),
+          to: 'font-awesome'
+        },
+        {
+          from: path.join(PROJECT_ROOT, 'node_modules/octicons'),
+          to: 'octicons'
+        }
+      ]),
+      new MonacoWebpackPlugin(initMonacoPluginConfig),
+      new HappyPack({
+        id: 'babel',
+        loaders: ['babel-loader?cacheDirectory'],
+        threadPool: happyThreadPool,
+        verbose: true
+      })
+    ],
+    module: {
+      rules: [
+        { test: /\.jsx?$/, exclude: /node_modules/, loader: 'happypack/loader?id=babel' },
+        { test: /\.md$/, use: ['raw-loader'] }
+      ]
     }
-  },
-  resolveLoader: {
-    modules: [path.resolve(__dirname, './loaders/'), 'node_modules']
-  },
-  node: {
-    fs: 'empty',
-    child_process: 'empty',
-    net: 'empty',
-    crypto: 'empty',
-  },
-  plugins: [
-    gitRevisionPlugin,
-    // new BundleAnalyzerPlugin(),
-    new DefinePlugin({
-      __VERSION__: str(gitRevisionPlugin.commithash() + '@' + gitRevisionPlugin.version()),
-      __PUBLIC_PATH__: str(publicPath),
-    }),
-    // https://github.com/kevlened/copy-webpack-plugin
-    new CopyWebpackPlugin([{
-      from: path.join(PROJECT_ROOT, 'static'),
-    }, {
-      from: path.join(PROJECT_ROOT, 'node_modules/font-awesome'),
-      to: 'font-awesome',
-    }, {
-      from: path.join(PROJECT_ROOT, 'node_modules/octicons'),
-      to: 'octicons',
-    }]),
-    new MonacoWebpackPlugin(initMonacoPluginConfig),
-    new HappyPack({
-      id: 'babel',
-      loaders: ['babel-loader?cacheDirectory'],
-      threadPool: happyThreadPool,
-      verbose: true
-    })
-  ],
-  module: {
-    rules: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'happypack/loader?id=babel' },
-      { test: /\.md$/, use: ['raw-loader'] }
-    ]
   }
-}
 }
