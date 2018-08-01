0893e07cc97dfc725d2234b2f1e826b722682251
veedrin <veedrin@qq.com>
2018-08-01 11:52:09 +0800

    add html preview editor

--- app/components/MonacoEditor/Editors/actions.js
+++ app/components/MonacoEditor/Editors/actions.js
@@ -12,6 +12,17 @@ export const editorResize = registerAction(MARKDOWN_EDITOR_RESIZE_MONACO,
   }
 )
 
+const HTML_EDITOR_RESIZE_MONACO = 'HTML_EDITOR_RESIZE_MONACO'
+export const htmlEditorResize = registerAction(HTML_EDITOR_RESIZE_MONACO,
+  (__, dX, dY, state) => ({ dX, dY, state }),
+  ({ dX, dY, state }) => {
+    const leftDom = document.getElementById('editor_preview_html_editor')
+    const rightDom = document.getElementById('editor_preview_html_preview')
+    state.leftGrow = state.leftGrow * (leftDom.offsetWidth - dX) / leftDom.offsetWidth
+    state.rightGrow = state.rightGrow * (rightDom.offsetWidth + dX) / rightDom.offsetWidth
+  }
+)
+
 export const MARKDOWN_EDITOR_TOGGLE_PREVIEW_MONACO = 'MARKDOWN_EDITOR_TOGGLE_PREVIEW_MONACO'
 export const togglePreview = registerAction(MARKDOWN_EDITOR_TOGGLE_PREVIEW_MONACO,
   ({ state }) => state.showPreview = !state.showPreview
