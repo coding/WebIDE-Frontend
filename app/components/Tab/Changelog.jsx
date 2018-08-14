cbd74a934c0737c6e2a4477b6c0dda67c5c4fe2c
veedrin <veedrin@qq.com>
2018-08-14 15:06:31 +0800

    remove marked

--- app/components/Tab/Changelog.jsx
+++ app/components/Tab/Changelog.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Remarkable from 'remarkable'
 import md from '../../../static/changelog/changelog.md'
 
 const style = {
@@ -6,8 +7,19 @@ const style = {
   marginTop: '2em'
 }
 
+const remarkable = new Remarkable('full', {
+  html: true,
+  xhtmlOut: false,
+  breaks: false,
+  langPrefix: 'language-',
+  linkify: true,
+  linkTarget: '_blank',
+  typographer: false,
+  quotes: '“”‘’',
+})
+
 const Changelog = () => (
-  <div dangerouslySetInnerHTML={{ __html: marked(md) }}
+  <div dangerouslySetInnerHTML={{ __html: remarkable.render(md) }}
     className='welcome-page' style={style}
   >
   </div>
