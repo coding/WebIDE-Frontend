2143e200c867326f48f56aa4602b978d3ab4163e
Yang Zhen <yangzhen@coding.net>
2018-08-14 10:43:25 +0800

    Fix error info

--- app/containers/Initialize/index.jsx
+++ app/containers/Initialize/index.jsx
@@ -150,6 +150,18 @@ class Initialize extends Component {
               <button className='btn btn-primary' onClick={() => window.location.href = '/ws/default'} >{i18n`global.returnDefault`}</button>
             </div>
           )
+        } else if (state.errorInfo.includes('your online workspace is limit to')) {
+          const limit = state.errorInfo.substring(state.errorInfo.length - 1)
+          info = (
+            <div className='loading-info'>
+              {i18n`global.workspaceOnlineLimit${{ limit }}`}
+            </div>
+          )
+          requestInfo = (
+            <div className='request-info'>
+              <button className='btn btn-primary' onClick={() => window.location.href = '/ws/default'} >{i18n`global.returnDefault`}</button>
+            </div>
+          )
         } else {
           info = (
             <div className='loading-info'>
