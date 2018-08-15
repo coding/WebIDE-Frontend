947073aa857a5faf09bc15ddf63d35d507941370
veedrin <veedrin@qq.com>
2018-08-15 17:50:01 +0800

    button's event handler switch to close web page when workspace limit has exceeded

--- app/containers/Initialize/index.jsx
+++ app/containers/Initialize/index.jsx
@@ -147,7 +147,7 @@ class Initialize extends Component {
           )
           requestInfo = (
             <div className='request-info'>
-              <button className='btn btn-primary' onClick={() => window.location.href = '/ws/default'} >{i18n`global.returnDefault`}</button>
+              <button className='btn btn-primary' onClick={() => { window.opener = null; window.open('', '_self'); window.close(); }} >{i18n`global.closeCurrentWebPage`}</button>
             </div>
           )
         } else if (state.errorInfo.includes('your online workspace is limit to')) {
@@ -159,7 +159,7 @@ class Initialize extends Component {
           )
           requestInfo = (
             <div className='request-info'>
-              <button className='btn btn-primary' onClick={() => window.location.href = '/ws/default'} >{i18n`global.returnDefault`}</button>
+              <button className='btn btn-primary' onClick={() => { window.opener = null; window.open('', '_self'); window.close(); }} >{i18n`global.closeCurrentWebPage`}</button>
             </div>
           )
         } else {
