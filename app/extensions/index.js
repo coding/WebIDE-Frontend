ca138fc148e158954238a25afb65cbbea2c6d867
sakuraash <841105127@qq.com>
2018-10-24 15:26:56 +0800

    fix file name

--- app/extensions/index.js
+++ app/extensions/index.js
@@ -1,11 +1,11 @@
-import * as file from './file.ext.js'
-import * as git from './git.ext.js'
-import * as editor from './editor.ext.js'
-import * as menu from './menu.ext.js'
+import * as file from './File.ext.js'
+import * as git from './Fit.ext.js'
+import * as editor from './Editor.ext.js'
+import * as menu from './Menu.ext.js'
 import * as command from './command'
-import * as modal from './modal.ext.js'
+import * as modal from './Modal.ext.js'
 import * as actions from './actions'
-import * as terminal from './terminal.ext.js'
+import * as terminal from './Terminal.ext.js'
 import PluginApp, { appRegistry, registerPluginConfiguration, getPluginConfiguration, registerPluginConfigChangeHandler, IPropertiesType } from './PluginApp.ext.js'
 
 export default {
