d3bfcdf98b8433f424d47d6aad5d80c3f2e771bf
Yang Zhen <yangzhen@coding.net>
2018-10-31 11:46:14 +0800

    Change cdn

--- app/containers/Footer.jsx
+++ app/containers/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => (
   <div id='global_footer'>
   <div className='record'>
     <div className='record_img_info'>
-      <a href='https://coding.net/index'><img src='https://dn-coding-net-production-pp.qbox.me/0905c8a9-5b33-4819-83d4-3cd0528b0c86.png' height='36' width='353' style={{ position: 'absolute' }} /></a>
+      <a href='https://coding.net/index'><img src='https://dn-coding-net-production-pp.codehub.cn/0905c8a9-5b33-4819-83d4-3cd0528b0c86.png' height='36' width='353' style={{ position: 'absolute' }} /></a>
       <div className='record_info'>
         <p>
           <a href='https://coding.net/about' target='_blank' rel='noopener noreferrer'>关于我们</a> | <a href='https://coding.net/privacy' target='_blank' rel='noopener noreferrer'>隐私策略</a> | <a href='https://coding.net/terms' target='_blank' rel='noopener noreferrer'>服务条款</a> | <a href='https://coding.net/security' target='_blank' rel='noopener noreferrer'>安全策略</a>
