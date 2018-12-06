45890c2e17354968ed407975f7f1943b653062c3
sakuraash <841105127@qq.com>
2018-12-06 14:29:28 +0800

    update lsp setting

--- app/settings.js
+++ app/settings.js
@@ -404,67 +404,24 @@ const settings = observable({
     _keys: ['projectType', 'sourcePath'],
     requireConfirm: true,
     confirmCallBack ([lang, path]) {
-      const prevFolder = config._ROOT_URI_
       const prevLang = config.mainLanguage
-      config.mainLanguage = lang
-      if (path !== '/') {
-        config._WORKSPACE_SUB_FOLDER_ = path
-        config._ROOT_URI_ = `/data/coding-ide-home/workspace/${config.spaceKey}/working-dir${path}`
-      }
-      const client = LanguageState.clients.get(prevLang)
-      if (client) {
-        if (lang !== prevLang) {
-          client.destory().then(() => createLanguageClient(lang))
-        } else {
-          client.workSpaceFoldersChange({
-            event: {
-              added: [
-                {
-                  uri: `file://${config._ROOT_URI_}`,
-                  name: `JAVA-PROJECT-FOLDER-${config._ROOT_URI_}`
-                }
-              ],
-              removed: [{ uri: `file://${prevFolder}`, name: `JAVA-PROJECT-FOLDER-${prevFolder}` }]
-            }
-          })
-        }
-      } else {
-        setLanguageServerOne({ type: lang, srcPath: path }).then((res) => {
-          if (res.code === 0) {
-            const prevFolder = config._ROOT_URI_
-            const prevLang = config.mainLanguage
-            config.mainLanguage = lang
-            if (path !== '/') {
-              config._WORKSPACE_SUB_FOLDER_ = path
-              config._ROOT_URI_ = `/data/coding-ide-home/workspace/${
-                config.spaceKey
-              }/working-dir${path}`
-            }
-            const client = LanguageState.clients.get(prevLang)
-            if (client) {
-              if (lang !== prevLang) {
-                client.destory().then(() => createLanguageClient(lang))
-              } else {
-                client.workSpaceFoldersChange({
-                  event: {
-                    added: [
-                      {
-                        uri: `file://${config._ROOT_URI_}`,
-                        name: `JAVA-PROJECT-FOLDER-${config._ROOT_URI_}`
-                      }
-                    ],
-                    removed: [
-                      { uri: `file://${prevFolder}`, name: `JAVA-PROJECT-FOLDER-${prevFolder}` }
-                    ]
-                  }
-                })
-              }
-            } else {
-              createLanguageClient(lang)
-            }
+      setLanguageServerOne({ type: lang, srcPath: path }).then((res) => {
+        if (res.code === 0) {
+          config.mainLanguage = lang
+          if (path !== '/') {
+            config._WORKSPACE_SUB_FOLDER_ = path
+            config._ROOT_URI_ = `/data/coding-ide-home/workspace/${
+              config.spaceKey
+            }/working-dir${path}`
           }
-        })
-      }
+          const client = LanguageState.clients.get(prevLang)
+          if (lang === prevLang || client) {
+            client.destory().then(() => createLanguageClient(lang))
+          } else {
+            createLanguageClient(lang)
+          }
+        }
+      })
     },
     projectType: {
       name: 'modal.projectType',
